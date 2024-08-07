a2dffe410b5ef8f6acecbb071c2bdd29e5e527bc
Mario David González <elavinchodev@gmail.com>
2024-08-06 22:36:19 -0300

    Codigo Base para Backend
    Ya tiene configurada la Base de Datos

---
+++ Backend/GestionDeInscripciones/src/main/java/com/PoloIT/GestionDeInscripciones/GestionDeInscripcionesApplication.java
@@ -0,0 +1,21 @@
+package com.PoloIT.GestionDeInscripciones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionDeInscripcionesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionDeInscripcionesApplication.class, args);
+                
+                System.out.println("");
+                System.out.println("");
+                System.out.println("** ----------- * POLO IT * ------------ **");
+                System.out.println("");
+                System.out.println("** ----- * Bienvenidos Squad 11 * ----- **");
+                System.out.println("");
+                System.out.println("Proyecto: Gestión de Inscripciones");
+	}
+
+}

43cc785e4eee5c952e2274f5dfe47d27815759e6
cristianA <cristianalbornoz2610@gmail.com>
2024-08-23 14:58:54 -0300

    .

--- back/src/main/java/com/PoloIT/GestionDeInscripciones/Services/TeamsServiceImpl.java
+++ back/src/main/java/com/PoloIT/GestionDeInscripciones/Services/TeamsServiceImpl.java
@@ -3,6 +3,7 @@ package com.PoloIT.GestionDeInscripciones.Services;
 import com.PoloIT.GestionDeInscripciones.Config.ExecptionControll.ResponseException;
 import com.PoloIT.GestionDeInscripciones.DTO.TeamsDTO;
 import com.PoloIT.GestionDeInscripciones.Entity.Event;
+import com.PoloIT.GestionDeInscripciones.Entity.Mentor;
 import com.PoloIT.GestionDeInscripciones.Entity.Student;
 import com.PoloIT.GestionDeInscripciones.Entity.Team;
 import com.PoloIT.GestionDeInscripciones.Repository.EventRepository;
@@ -31,26 +32,81 @@ public class TeamsServiceImpl {
     private Set<Team> createTeamGroup(Event event, TeamsDTO teamsDTO) {
 
         List<Team> teams = new ArrayList<>();
-        int integrantLimit = (int) Math.ceil((double) event.getRegistration().getStudents().size() / teamsDTO.groups());
-        int count = 0;
-        System.out.println("count " + count);
+        int memberLimit = (int) Math.ceil((double) (event.getRegistration().getStudents().size() + event.getRegistration().getMentors().size()) / teamsDTO.groups());
+        //  int count = 0;
+        int countStudent = 0;
+        int countMentor = 0;
 
-        for (int i = 0; i < teamsDTO.groups(); i++) {
+        //poder ver si alguien se queda afuer
+        int e = (event.getRegistration().getStudents().size() + event.getRegistration().getMentors().size());
+
+        //  for (int i = 0; i <); i++) {
+        for (int i = 0; i <= e; i++) {
+
             Team team = new Team();
 
-            for (int j = 0; j < integrantLimit; j++) {
-                if ((j + count) >= event.getRegistration().getStudents().size())
+            int limitStudent = memberLimit - (memberLimit - teamsDTO.limitStudent());
+            int limitMentor = memberLimit - (memberLimit - teamsDTO.limitMentor());
+            System.out.println("------------");
+            System.out.println("memberLimit " + memberLimit);
+            System.out.println("limitMentor " + limitMentor);
+            System.out.println("limitStudent " + limitStudent);
+
+            for (int j = 0; j < limitStudent; j++) {
+
+                int index = j + countStudent;
+                System.out.println("student " + event.getRegistration().getStudents().size());
+                System.out.println("index " + index);
+
+                if (index >= event.getRegistration().getStudents().size())
                     break;
-                Student student = event.getRegistration().getStudents().stream().toList().get(j + count);
+                Student student = event.getRegistration().getStudents().stream().toList().get(index);
                 team.getStudents().add(student);
             }
 
-            count += integrantLimit;
+            for (int j = 0; j < limitMentor; j++) {
+                int index = j + countMentor;
+                System.out.println("metors " + event.getRegistration().getMentors().size());
+                System.out.println("index " + index);
+                if (index >= event.getRegistration().getMentors().size())
+                    break;
+                Mentor mentor = event.getRegistration().getMentors().stream().toList().get(index);
+                team.getMentors().add(mentor);
+            }
+            System.out.println("------------");
 
+            //   count += memberLimit;
+
+            countMentor += limitMentor;
+            countStudent += limitStudent;
+
+            System.out.println("aaaaaaaaaaaaaaaaaaaaaaaaaa");
+            System.out.println(team.getMentors().size());
+            System.out.println(team.getStudents().size());
+            System.out.println("aaaaaaaaaaaaaaaaaaaaaaaaaa");
+
+            // si tems la lista de student y mentor esta vacia significa que no hay que crear mas grupos.
+            if (team.getStudents().isEmpty() && team.getMentors().isEmpty()) {
+                break;
+            }
             teams.add(team);
         }
 
-        System.out.println(teams.size());
+        // ver las incriptos que queador fuera.
+        // con el lime de grupo sacamos de todo los primo grupos.
+
+
+        teams.forEach((x) -> {
+            System.out.println("team " + x.getId());
+            x.getStudents().forEach(student -> {
+                System.out.println(student.getId() + " " + student.getName());
+
+            });
+            x.getMentors().forEach(mentor -> {
+                System.out.println(mentor.getId() + " " + mentor.getName());
+            });
+        });
+
         return null;
     }
 

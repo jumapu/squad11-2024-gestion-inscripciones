acfd750dd1dbaaa77d3a450dd57a7b83483254fa
Frere-Luz <algo_de_luz@hotmail.com>
2024-08-28 19:18:04 -0300

    pruebas de request login

---
+++ frontend/src/api/apiClient.js
@@ -0,0 +1,27 @@
+// import axios from "axios";
+
+// const apiClient = axios.create({
+//     baseURL: 'http://localhost:8080/api',
+//     httpsAgent: false, // Disable HTTPS
+//     headers: {
+//       'Content-Type': 'application/json',
+//     },
+//   },
+//   apiClient.interceptors.response.use(
+//     response => response,
+//     error => {
+//       console.error('API call failed:', error);
+//       // Handle specific error cases
+//       if (error.response.status === 401) {
+//         // Unauthorized
+//       } else if (error.response.status === 404) {
+//         // Not found
+//       }
+//       return Promise.reject(error);
+//     }
+//   )
+// );
+
+// export default apiClient;
+
+

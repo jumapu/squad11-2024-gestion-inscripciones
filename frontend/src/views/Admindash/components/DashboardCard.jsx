7a070f6eb3a924feeb3a8af54beab30a72b9023a
Frere-Luz <algo_de_luz@hotmail.com>
2024-09-17 22:14:20 -0300

    corrijo push anterior en DshCard

--- frontend/src/views/Admindash/components/DashboardCard.jsx
+++ frontend/src/views/Admindash/components/DashboardCard.jsx
@@ -2,6 +2,12 @@ import Eventos from "./Eventos";
 import Mentores from "./Mentores";
 import Stack from "@mui/material/Stack";
 
+const DashboardCard = () => {
+  return (
+    <div>
+      <Stack spacing={2}>
+      <Eventos/>
+      <Mentores/>
       </Stack>
     </div>
   )

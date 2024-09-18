27b18e03dc52b98aac1000cbd8db15492596df70
Frere-Luz <algo_de_luz@hotmail.com>
2024-09-18 17:42:22 -0300

    trabajo sobre tabla de Egresados y menu drawer

--- frontend/src/views/Admindash/components/EgresadosTable.jsx
+++ frontend/src/views/Admindash/components/EgresadosTable.jsx
@@ -6,118 +6,113 @@ import { IoPencil } from "react-icons/io5";
 import { IoTrashOutline } from "react-icons/io5";
 import { Link } from 'react-router-dom';
 import DataTable from 'react-data-table-component';
-
-
-const customStyles = {
-  table: {
-    style: {
-      width: '100%',
-      justifyContent: "around",
-      responsive: true,
-    }
-  },
-  headCells: {
-    style: {
-      fontSize: "1rem",
-      justifyContent: "center",
-    },
-    rows: {
-      style: {
-        height: '55px',
-        maxWidth: '98vw',
-        justify: 'center',
-        align: "center",
-        responsive: true,
-      },
-    }
-  },
-  cells: {
-    style: {
-      textAlign: "center",
-      justifyContent: 'center',
-      responsive: false,
-    }
-  },
-};
-
-const columns = [
-  {
-    name: 'ID',
-    selector: row => row.id,
-  },
-  {
-    name: 'Nombre',
-    selector: row => row.name,
-  },
-  {
-    name: 'Apellido',
-    selector: row => row.surname,
-  },
-  {
-    name: 'Rol',
-    selector: row => row.role,
-  },
-  {
-    name: 'Team',
-    selector: row => row.team,
-  },
-  {
-    name: 'Acciones',
-    cell: () =>
-      <div style={{ display: 'flex', alignItems: 'center' }}>
-        <Tooltip className='bg-slate-300' content="Informacion">
-          <Link to='/dashboard/egresados/informacionForm'>
-            <IconButton className='bg-slate-800 btn-grp' radius="full">
-              <IoIosInformation />
-            </IconButton>
-          </Link>
-        </Tooltip>
-        <Tooltip className='bg-slate-300' content="Modificar Informacion">
-          <Link to='/dashboard/egresados/informacionForm' />
-          <IconButton className='bg-slate-800 btn-grp editCita' radius="full" />
-          <IoPencil />
-        </Tooltip>
-        <Tooltip className='bg-slate-300' content="Borrar Informacion">
-          <Link to='/dashboard/egresados/informacionForm'>
-            <IconButton className='bg-slate-800 btn-grp' radius="full">
-              <IoTrashOutline />
-            </IconButton>
-          </Link>
-        </Tooltip>
-
-      </div>
-  },
-];
-
-const datos = [
-
-]
+import axiosInstance from "@/api/interceptor.js";
 
 export const EgresadosTable = () => {
+  const drawerWidth = 240;
+  const customStyles = {
+    table: {
+      style: {
+        width: "100%", 
+        maxWidth: "1440px",
+      }
+    },
+    headCells: {
+      style: {
+        fontSize: "1rem",
+        justifyContent: "center",
+      }
+    },
+    cells: {
+      style: {
+        textAlign: "center",
+      }
+    }
+  };
+
+  const columns = [
+    {
+      name: 'ID',
+      selector: row => row.id,
+    },
+    {
+      name: 'Nombre',
+      selector: row => row.name,
+    },
+    {
+      name: 'Apellido',
+      selector: row => row.surname,
+    },
+    {
+      name: 'Rol',
+      selector: row => row.rol,
+    },
+    {
+      name: 'Team',
+      selector: row => row.team,
+    },
+    {
+      name: 'Acciones',
+      cell: row => (
+        <div style={{ display: 'flex', alignItems: 'center' }}>
+          <Tooltip content="Informacion">
+            <Link to={`Egresados/informacionForm/${row.id}`}>
+              <IconButton className='bg-slate-800'>
+                <IoIosInformation />
+              </IconButton>
+            </Link>
+          </Tooltip>
+          <Tooltip content="Modificar Informacion">
+            <Link to={`/dashboard/egresados/editar/${row.id}`}>
+              <IconButton className='bg-slate-800'>
+                <IoPencil />
+              </IconButton>
+            </Link>
+          </Tooltip>
+          <Tooltip content="Borrar Informacion">
+            <Link to={`/dashboard/egresados/borrar/${row.id}`}>
+              <IconButton className='bg-slate-800'>
+                <IoTrashOutline />
+              </IconButton>
+            </Link>
+          </Tooltip>
+        </div>
+      ),
+    },
+  ];
+
+  const [Estudiantes, setEstudiantes] = useState([]);
   const [pending, setPending] = useState(true);
-  const [rows, setRows] = useState([]);
+
   useEffect(() => {
-    const timeout = setTimeout(() => {
-      setRows(datos);
-      setPending(false);
-    }, 2000);
-    return () => clearTimeout(timeout);
+    const fetchEstudiantes = async () => {
+      try {
+        const result = await axiosInstance.get("admin/user/all");
+        const estudiantes = result.data.Student.Estudiantes || [];
+        console.log(result);
+        setEstudiantes(estudiantes);
+        setPending(false);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+
+    fetchEstudiantes();
   }, []);
+
   return (
-    <div>
+    <div style={{marginLeft: drawerWidth }}>
       <DataTable
         columns={columns}
-        data={datos}
+        data={Estudiantes}
         customStyles={customStyles}
-        expandableRows
         pagination
         paginationPerPage={10}
         progressPending={pending}
         responsive
-        onSelectedRowsChange={(data) => console.log(data)}
       />
     </div>
-  )
+  );
 }
 
-export default EgresadosTable
+export default EgresadosTable;

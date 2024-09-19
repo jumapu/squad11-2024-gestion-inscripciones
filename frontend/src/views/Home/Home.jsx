97069a60cc7d6f879c387fe8e09647f36f620f42
Frere-Luz <algo_de_luz@hotmail.com>
2024-09-19 18:07:52 -0300

    agrego correccion a  paleta de colores y modificaciones en paginas segun diseño

--- frontend/src/views/Home/Home.jsx
+++ frontend/src/views/Home/Home.jsx
@@ -11,21 +11,20 @@ export default function Home() {
     <div className="bg-white">
       <Navbar />
       <div className="bg-neutral-50 relative isolate max-h-screen px-6 pt-14 lg:px-8">
-        <div className=" max-w-2xl py-32 sm:py-48 lg:py-56">
-          <div className="text-start pt-14">
-            <h1 className="font-medium tracking-tight text-gray-900 text-3xl">
-              Lorem, ipsum dolor sit amet consectetur.
+        <div className=" max-w-xl lg:pl-32 py-32 sm:py-48 lg:py-56">
+          <div className="text-start pt-10">
+            <h1 className="font-extrabold tracking-tight text-gray-900 text-3xl">
+              Bienvenido al sistema de inscripcion del Polo IT.
             </h1>
-            <p className="mt-6 text-lg leading-8 text-gray-600">
-              Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet
-              fugiat veniam occaecat fugiat aliqua.
+            <p className="mt-4 text-base leading-5 text-gray-800">
+              Una plataforma innovadora que conecta automaticamente a mentores tecnicos con egresados, facilitando su participacion en diversas actividades y programas.
             </p>
           </div>
         </div>
       </div>
       <Features/>
-      <AboutUs/>
       <News/>
+      <AboutUs/>      
       <Reviews/>
       <Footer/>
       <ScrollToTopButton/>
--- frontend/src/views/Home/components/News.jsx
+++ frontend/src/views/Home/components/News.jsx
@@ -1,30 +1,28 @@
-import { Flex, Heading, Text, Button, Link } from "@radix-ui/themes";
+import { Flex, Heading, Text } from "@radix-ui/themes";
 const News = () => {
     return (
         <div id="novedades">
             <Flex direction="column" justify="center" px="5">
-                <Heading size="8" className="py-10 text-sky-800 mx-auto w-full sm:text-center lg:mx-0 lg:text-left">
-                    Proximos Programas
+                <Heading size="8" className="py-10 text-gray-800 mx-auto w-full sm:text-center lg:mx-0">
+                    Proximo Evento
                 </Heading>
-                <figure className="md:flex border-2 border-solid border-slate-200 rounded-xl p-8 md:p-0 items-center shadow-md shadow-slate-400">
+                <figure className="md:flex border-2 border-solid border-blue-800 rounded-xl p-8 md:p-0 items-center shadow-md shadow-slate-400">
                     <div className="pt-6 md:p-8 text-center md:text-left space-y-4">
                         <blockquote>
-                            <p className="text-3xl text-slate-600 font-medium text-center">
+                            <p className="text-3xl text-sky-800 font-medium text-center">
                                 10 agosto 2024
                             </p>
                         </blockquote>
                     </div>
                     <div className="pt-6 md:p-8 text-center md:text-left space-y-4">
-                        <Heading as="h1" size="6" className="text-sky-800">Acelerador</Heading>
-                        <Text as="div">Lorem ipsum, dolor sit amet consectetur adipisicing elit.</Text>
-                        <Text>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quam laboriosam voluptatem dicta cumque at nostrum, dolorem minima voluptates praesentium inventore porro reprehenderit est magni ex, velit quibusdam, nemo eius quod?</Text>
+                        <Heading as="h1" size="7" className="text-red-800  font-black tracking-wide leading-5">Acelerador IT</Heading>
+                        <Text as="div">Este programa ofrece un paquete exclusivo de contenidos técnicos asincrónicos y sesiones sincrónicas de acompañamiento profesional, brindadas por empresas socias del Polo IT. A lo largo del evento, trabajarás tanto en habilidades soft como en hard, con el objetivo de refinar y potenciar tu perfil.
+                        </Text>
+                        <Text as="div">
+                            Si ya tienes conocimientos en programación, pero aún no te sientes listo para dar el salto al mercado laboral en tu primera etapa profesional, El Acelerador es la oportunidad perfecta para preparar tu camino y alcanzar el siguiente nivel.
+                        </Text>
                     </div>
                 </figure>
-                <Flex className="gap-4 py-8 justify-around">
-                    <Link href='#'>
-                        <Button className="p-5 bg-sky-800 raduis=lg font-medium text-nowrap shadow-black shadow-md cursor-pointer text-lg">Mas Programas</Button>
-                    </Link>
-                </Flex>
             </Flex>
         </div >
     )
